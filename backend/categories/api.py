50dfabc86c35cb9c00110ef66fca798a9bf47537
rasmushaa <rasmus.haapaniemi@outlook.com>
2024-11-17 21:03:04 +0200

    Refactor categories

--- backend/categories/api.py
+++ backend/categories/api.py
@@ -5,28 +5,33 @@ class CategoriesAPI():
 
     def __init__(self):
         self.__client = GoogleCloudAPI()
-        self.__pull_data()
 
 
     def get_expenditure_categories(self):
-        return self.__expenditure_categories 
-    
-
-    def __pull_data(self):
         sql = f"""
         SELECT
             Name,
-            Type
         FROM 
             {self.__client._dataset}.d_category
+        WHERE
+            Type = 'expenditure'
         GROUP BY
-            1, 2 -- Group for case of duplication
+            1 -- Group for case of duplication
         """
-        df = self.__client.sql_to_pandas(sql)
-        self.__df = df
-        self.__refresh()
+        df = self.__client.sql_to_pandas(sql) 
+        return df['Name'].to_list()
+    
 
-
-    def __refresh(self):
-        self.__expenditure_categories = self.__df.loc[self.__df['Type'] == 'expenditure', 'Name'].to_list()
-        self.__asset_categories = self.__df.loc[self.__df['Type'] == 'asset', 'Name'].to_list()
+    def get_asset_categories(self):
+        sql = f"""
+        SELECT
+            Name,
+        FROM 
+            {self.__client._dataset}.d_category
+        WHERE
+            Type = 'asset'
+        GROUP BY
+            1 -- Group for case of duplication
+        """
+        df = self.__client.sql_to_pandas(sql) 
+        return df['Name'].to_list()
